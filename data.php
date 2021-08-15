bf5687ff33fea05ba32895475dfe63c8df29c858
URIAN VIERA <urian1213viera@gmail.com>
2021-08-14 21:43:53 -0500

    eliminando archivo no necesario

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cantidad            = $_REQUEST['cantidad'];
-
-$quitarString        = array(",", "."); 
-$sinStrinPrecio      = str_replace($quitarString, "", $cantidad);
-$cantidadFormateada  =  number_format($sinStrinPrecio, 0,'','.');
-
-echo '<strong>$</strong> '. $cantidadFormateada ;
-?>
